ba65ffaa0acef14eecc506710b4227699ca51b6d
joshuafouch <joshfouch777@gmail.com>
2025-06-15 13:09:29 -0800

    Updates

---
+++ _app/immutable/nodes/2.B37zPzt-.js
@@ -0,0 +1,1 @@
+import{f as o,a}from"../chunks/BoVA4qQb.js";import"../chunks/CK1MN0Xm.js";import{n as r}from"../chunks/azicADLO.js";var m=o('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=m();r(2),a(t,e)}export{s as component};
